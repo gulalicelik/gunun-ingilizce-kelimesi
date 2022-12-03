4aeca22cb9d71ea16dfa7d073963fc4cdd4ff0bb
gulalicelik <gulalicelikmi@gmail.com>
2022-12-03 14:48:17 +0300

    add validator for sign in and up

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,10 +1,9 @@
 const db = require("../models");
 const User = db.user;
 const bcrypt = require('bcrypt');
+const validator = require('../utility/validators.js');
 
-
-const signup = async (req, res) => {
-
+const signUp = async (req, res) => {
     // user signup using email and password bcrypt
     const {firstname, lastname, username, email, password} = req.body;
     const salt = bcrypt.genSaltSync(10);
@@ -34,10 +33,30 @@ const signup = async (req, res) => {
         "message": "User already exists",
         "email"   : row.email,
     })
+}
 
-
+const signIn = async (req, res) => {
+    // user signin using email and password bcrypt
+    const {email, password} = req.body;
+    const user = await User.findOne({where: {email}});
+    if (user) {
+        const validPassword = bcrypt.compareSync(password, user.password);
+        if (validPassword) {
+            res.send({
+                "status" : "success",
+                "message": "User logged in successfully",
+                data     : user
+            })
+            return;
+        }
+    }
+    res.send({
+        "status" : "error",
+        "message": "Invalid email or password",
+    })
 }
 
 module.exports = {
-    signup
+    signUp,
+    signIn
 }
