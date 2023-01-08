ad28876c8abaa5445b7cf760f5fc8057467ae9f5
gulalicelik <gulalicelikmi@gmail.com>
2023-01-08 22:43:36 +0300

    addes pm2 and generate ecosystem config file

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [{
+    script  : 'app.js',
+    watch   : true,
+    env_dev : {
+      name    : 'gik-development',
+      NODE_ENV: 'development',
+      PORT    : 3000,
+    },
+    env_test: {
+      name    : "gik-test",
+      PORT    : 3001,
+      NODE_ENV: "test",
+    },
+    env_prod: {
+      name    : "gik-prod",
+      PORT    : 3002,
+      NODE_ENV: "production",
+    },
+  }],
+};
+
+
